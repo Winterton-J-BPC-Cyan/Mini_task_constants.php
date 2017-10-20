1f9cd4c69341850851a7143b7517fb312b2e7035
Josh winterton <32195114+Winterton-J-BPC-Cyan@users.noreply.github.com>
2017-10-20 09:50:23 +0100

    Mini_task_constants.php
    Adding the first test file

---
+++ Mini_task_constants.php
@@ -0,0 +1,31 @@
+<html>
+
+<body>
+
+<?php
+define("firstName","Josh");
+define("secondName","Winterton");
+define("favSubject","3D modeling");
+$age="18 years old";
+
+echo "My first name is: ";
+echo firstName;
+echo "'<br/><br/>'";
+echo "My surname is: ";
+echo secondName;
+echo "'<br/><br/>'";
+echo "My favourite subject is: ";
+echo favSubject;
+echo "'<br/><br/>'";
+echo "I am currently: ";
+echo $age;
+echo "'<br/><br/>'";
+echo "My age add five years is: ";
+echo $age + "5";
+echo "'<br/><br/>'";
+
+?>
+
+</body>
+
+</html>
