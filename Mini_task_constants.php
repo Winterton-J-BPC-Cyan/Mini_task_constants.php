57b97ff122a1f46c10d19426f5aeee76d60a7713
Josh winterton <32195114+Winterton-J-BPC-Cyan@users.noreply.github.com>
2017-10-20 10:26:42 +0100

    Update Mini_task_constants.php

--- Mini_task_constants.php
+++ Mini_task_constants.php
@@ -7,6 +7,7 @@ define("firstName","Josh");
 define("secondName","Winterton");
 define("favSubject","3D modeling");
 $age="18 years old";
+$browser = $_SERVER['HTTP_USER_AGENT'];
 
 echo "My first name is: ";
 echo firstName;
@@ -23,7 +24,9 @@ echo "<br/><br/>";
 echo "My age add five years is: ";
 echo $age + "5";
 echo "<br/><br/>";
-echo $GLOBAL;
+echo $browser;
+
+
 ?>
 
 </body>
